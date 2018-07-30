e841e871693fd1fc9d61794cceeae75583d4a7d3
seanyinx <seanyinx@gmail.com>
2018-07-30 21:37:17 +0800

    added spring controller test

---
+++ sprint-boot/src/main/java/com/github/seanyinx/testbed/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.github.seanyinx.testbed;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(Exception.class)
+  ResponseEntity<String> handleException(Exception ex) {
+    return new ResponseEntity<>(INTERNAL_SERVER_ERROR);
+  }
+}
---
+++ sprint-boot/src/main/java/com/github/seanyinx/testbed/UserController.java
@@ -0,0 +1,32 @@
+package com.github.seanyinx.testbed;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.github.seanyinx.testbed.base.domain.User;
+import com.github.seanyinx.testbed.base.services.UserService;
+
+@Controller
+@RequestMapping("/users")
+class UserController {
+
+  private final UserService userService;
+
+  @Autowired
+  public UserController(UserService userService) {
+    this.userService = userService;
+  }
+
+  @GetMapping("/{id}")
+  User getUser(@PathVariable long id) {
+    Optional<User> user = userService.findUser(id);
+
+    return user.orElseThrow(() -> new NoSuchElementException("No such user with id " + id));
+  }
+}
