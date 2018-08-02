065a72f9677c2f00d52bfb37eec5d9cd5c2def07
seanyinx <seanyinx@gmail.com>
2018-08-02 22:01:56 +0800

    solution for docker based integration tests

--- integration/src/test/java/com/github/seanyinx/testbed/Session1_DockerBasedTest.java
+++ integration/src/test/java/com/github/seanyinx/testbed/Session1_DockerBasedTest.java
@@ -2,13 +2,18 @@ package com.github.seanyinx.testbed;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.ClassRule;
 import org.junit.Test;
 
+import com.github.seanyinx.testbed.containers.RedisContainer;
+
 public class Session1_DockerBasedTest {
+  @ClassRule
+  public static final RedisContainer redis = new RedisContainer();
 
   @Test
   public void shouldFetchDataFromCache() throws Exception {
-    RedisCache cache = new RedisCache("localhost", 6379);
+    RedisCache cache = new RedisCache(redis.getContainerIpAddress(), redis.getMappedPort(6379));
 
     cache.put("foo", "bar");
     String value = cache.get("foo");
