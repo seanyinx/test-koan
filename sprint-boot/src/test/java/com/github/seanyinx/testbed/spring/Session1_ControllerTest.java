d22788652192e91e44d08e53048a0e89f7f8268d
seanyinx <seanyinx@gmail.com>
2020-06-01 10:18:49 +0800

    remove user service mocking

--- sprint-boot/src/test/java/com/github/seanyinx/testbed/spring/Session1_ControllerTest.java
+++ sprint-boot/src/test/java/com/github/seanyinx/testbed/spring/Session1_ControllerTest.java
@@ -1,21 +1,15 @@
 package com.github.seanyinx.testbed.spring;
 
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-import java.util.Optional;
-
+import com.github.seanyinx.testbed.base.domain.User;
+import com.github.seanyinx.testbed.spring.interfaces.UserController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
-import com.github.seanyinx.testbed.base.domain.User;
-import com.github.seanyinx.testbed.base.services.UserService;
-import com.github.seanyinx.testbed.spring.interfaces.UserController;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(UserController.class)
@@ -25,21 +19,18 @@ public class Session1_ControllerTest {
   @Autowired
   private MockMvc mockMvc;
 
-  @MockBean
-  private UserService userService;
-
   @Test
   public void shouldReturnExpectedUser() throws Exception {
-    when(userService.findUser(1L)).thenReturn(Optional.of(jack));
 
+    // TODO: mock behavior of user service
     // TODO: check response status and response body
     mockMvc.perform(get("/users/{id}", 1));
   }
 
   @Test
   public void shouldReturn500() throws Exception {
-    when(userService.findUser(1L)).thenReturn(Optional.empty());
 
+    // TODO: mock behavior of user service
     // TODO: check response status and response body
     mockMvc.perform(get("/users/{id}", 1));
   }
